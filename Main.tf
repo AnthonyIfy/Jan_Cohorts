0c48dab89ec2b1c25510d4479a259fedecec99b6
AnthonyIfy <136186728+AnthonyIfy@users.noreply.github.com>
2025-03-28 15:33:10 -0500

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,7 +1,7 @@
 # configured aws provider with proper credentials
 provider "aws" {
-  region     = "us-east-1"
-  profile    = "yusuf"
+  region     = "us-west-1"
+  profile    = "asher"
 }
 
 
@@ -21,7 +21,7 @@ resource "aws_vpc" "prodvpc" {
 resource "aws_subnet" "prodsubnet1" {
   vpc_id            = aws_vpc.prodvpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "us-west-1a"
   map_public_ip_on_launch = true
   
   tags = {
@@ -123,8 +123,8 @@ resource "aws_instance" "firstinstance" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_web.id]
   subnet_id              = aws_subnet.prodsubnet1.id
-  key_name               = "devopskeypair"
-  availability_zone      = "us-east-1a"
+  key_name               = "deployment"
+  availability_zone      = "us-west-1a"
   user_data              =  "${file("install_jenkins.sh")}"
 
 
@@ -140,7 +140,7 @@ resource "aws_instance" "secondinstance" {
   vpc_security_group_ids = [aws_security_group.allow_web.id]
   subnet_id              = aws_subnet.prodsubnet1.id
   key_name               = "devopskeypair"
-  availability_zone      = "us-east-1a"
+  availability_zone      = "us-west-1a"
   user_data              =  "${file("install_tomcat.sh")}"
   
 
