17e11fa9c67ba11b83aef54199a6b7a9e663c267
AnthonyIfy <136186728+AnthonyIfy@users.noreply.github.com>
2025-03-28 15:37:33 -0500

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -139,7 +139,7 @@ resource "aws_instance" "secondinstance" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_web.id]
   subnet_id              = aws_subnet.prodsubnet1.id
-  key_name               = "devopskeypair"
+  key_name               = "deployment"
   availability_zone      = "us-west-1a"
   user_data              =  "${file("install_tomcat.sh")}"
   
